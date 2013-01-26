8c51b2e180edcab217a62f028fbc925e4e1a786d
Samuel CHEVET <gw4kfu@gmail.com>
2013-01-26 18:36:28 +0100

    Adding extract functionality

--- snd_extractor/main.c
+++ snd_extractor/main.c
@@ -5,126 +5,70 @@ LPTSTR		lpszDialogCaption = "Snd Extractor";
 struct      file sFile;
 HWND        hListView;
 
-LRESULT CALLBACK MainProc(HWND Dlg,UINT message,WPARAM wParam,LPARAM lParam);
-void open_snd(HWND hWin);
+LRESULT CALLBACK MainProc(HWND Dlg, UINT message, WPARAM wParam, LPARAM lParam);
+void OpenSndFile(HWND hWin);
 void SetFileInformation(HWND hWin, OPENFILENAME *ofn);
-void add_item(DWORD dwNum, struct entry_wav *sWav);
-void extract_file(struct file *sFile, DWORD dwPos);
-
+void AddItem(DWORD dwNum, struct entry_wav *sWav);
+void ExtractFile(struct file *sFile, DWORD dwPos);
+DWORD GetNbEntry(struct file *sFile);
+void MakeMenu(HWND hWin);
+void ExtractAction(void);
+void PopMenuList(HWND hWin, LPARAM lParam);
+void FixWavNameFile(PBYTE Buf);
 
 int APIENTRY WinMainCRTStartup()
 {
-   INITCOMMONCONTROLSEX icex;
+    //INITCOMMONCONTROLSEX icex;
 
     hInst = (HINSTANCE)GetModuleHandleA(NULL);
 	InitCommonControls();
 
-    icex.dwICC = ICC_LISTVIEW_CLASSES;
-    InitCommonControlsEx(&icex);
+    //icex.dwICC = ICC_LISTVIEW_CLASSES;
+    //InitCommonControlsEx(&icex);
 
 	DialogBoxParam(hInst, (LPCTSTR)IDD_DIALOG1, NULL, (DLGPROC)MainProc, 0);
 	ExitProcess (0);
     return 0;
 }
 
-LRESULT CALLBACK MainProc(HWND hWin,UINT message,WPARAM wParam,LPARAM lParam)
+LRESULT CALLBACK MainProc(HWND hWin, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    int Select;
-    HMENU hMenu, hSubMenu;
-    LVCOLUMN lvc = {0};
-
+    DWORD dwSelect;
 
     switch(message)
     {
         case WM_INITDIALOG:
-            hMenu = CreateMenu();
-            hSubMenu = CreatePopupMenu();
-            AppendMenu(hSubMenu, MF_STRING, ID_OPEN, "O&pen");
-            AppendMenu(hSubMenu, MF_STRING, ID_QUIT, "E&xit");
-            AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&File");
-
-            AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)NULL, "&About");
-
-            SetMenu(hWin, hMenu);
-
-			SendMessage (hWin, WM_SETTEXT, 0, (LPARAM)lpszDialogCaption);
+            SendMessage(hWin, WM_SETTEXT, 0, (LPARAM)lpszDialogCaption);
+            SetDlgItemText(hWin, IDC_STATIC, "No File Loaded");
 			//HICON hIcon = LoadIcon (hInst, (LPCSTR)IDI_ICON1);
 			//SendMessage (hWin, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
-			SetDlgItemText (hWin, IDC_STATIC, "No File Loaded");
-
-            //SendMessage(hWin, LB_SETCOLUMNWIDTH, (WPARAM) 100, (LPARAM) 0);
-            hListView = CreateWindow(WC_LISTVIEW, NULL,
-                                 WS_CHILD | LVS_REPORT |WS_VISIBLE,
-                                 10, 10, 380, 440,
-                                 hWin, (HMENU) 104, hInst, NULL);
-            ListView_SetExtendedListViewStyle(hListView, LVS_EX_FULLROWSELECT | LVS_EX_HEADERDRAGDROP);
-            //ListView_SetExtendedListViewStyle(hListView, LVS_EX_CHECKBOXES | LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
-            lvc.mask = LVCF_TEXT | LVCF_SUBITEM | LVCF_WIDTH | LVCF_FMT;
-            lvc.fmt  = LVCFMT_CENTER;
-
-            EnableWindow(SendMessage(hListView,LVM_GETHEADER, 0, 0), FALSE);
-
-            lvc.iSubItem = 0;
-            lvc.cx       = 150;
-            lvc.pszText  = TEXT("Name");
-            ListView_InsertColumn(hListView, 0, &lvc);
-
-            lvc.iSubItem = 1;
-            lvc.cx       = 100;
-            lvc.pszText  = TEXT("Offset");
-            ListView_InsertColumn(hListView, 1, &lvc);
-
-            lvc.iSubItem = 2;
-            lvc.cx       = 100;
-            lvc.pszText  = TEXT("Size");
-            ListView_InsertColumn(hListView, 2, &lvc);
+            MakeMenu(hWin);
 			break;
         case WM_COMMAND:
-			Select = LOWORD(wParam);
-			switch(Select)
+			dwSelect = LOWORD(wParam);
+			switch(dwSelect)
 			{
 			    case ID_OPEN:
-                    open_snd(hWin);
+                    OpenSndFile(hWin);
+                    break;
+                case ID_EXTRACT:
+                    ExtractAction();
                     break;
 			    case ID_QUIT:
 				case IDC_QUIT:
                     SendMessage(hWin, WM_CLOSE, 0, 0);
                     break;
-                case ID_EXTRACT:
-                {
-                    int iPos = ListView_GetNextItem(hListView, -1, LVNI_SELECTED);
-                    if (iPos == -1)
-                    {
-                        MessageBoxA(NULL, "Load a file first", "ERROR", 0);
-                    }
-                    else
-                    {
-                        extract_file(&sFile, iPos);
-                    }
-                    break;
-                }
 				default:
 					break;
 			}
 			break;
         case WM_NOTIFY :
-        {
-            LPNMHDR pnmhdr;
-            pnmhdr = (LPNMHDR)lParam;
-            if (pnmhdr->hwndFrom == hListView && pnmhdr->code == NM_RCLICK)
-            {
-                POINT lpPoint;
-                hSubMenu = CreatePopupMenu();
-                AppendMenu(hSubMenu, MF_STRING, ID_EXTRACT, "E&xtract");
-                GetCursorPos(&lpPoint);
-                TrackPopupMenu(hSubMenu, TPM_LEFTALIGN , lpPoint.x, lpPoint.y,NULL, hWin, NULL);
-            }
+            PopMenuList(hWin, lParam);
             break;
-        }
         case WM_CLOSE:
         case WM_DESTROY:
-			ShowWindow (hWin, SW_MINIMIZE);
-			EndDialog (hWin,0);
+			ShowWindow(hWin, SW_MINIMIZE);
+			EndDialog(hWin,0);
 			break;
         default:
             return FALSE;
@@ -132,13 +76,77 @@ LRESULT CALLBACK MainProc(HWND hWin,UINT message,WPARAM wParam,LPARAM lParam)
    return TRUE;
 }
 
-void open_snd(HWND hWin)
+void MakeMenu(HWND hWin)
+{
+    HMENU hMenu, hSubMenu;
+    LVCOLUMN lvc = {0};
+
+    hMenu = CreateMenu();
+    hSubMenu = CreatePopupMenu();
+    AppendMenu(hSubMenu, MF_STRING, ID_OPEN, "O&pen");
+    AppendMenu(hSubMenu, MF_STRING, ID_QUIT, "E&xit");
+    AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&File");
+    AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)NULL, "&About");
+    SetMenu(hWin, hMenu);
+    hListView = CreateWindow(WC_LISTVIEW, NULL,
+                        WS_CHILD | LVS_REPORT |WS_VISIBLE | LVS_SINGLESEL,
+                        10, 10, 380, 440,
+                        hWin, (HMENU) 104, hInst, NULL);
+    ListView_SetExtendedListViewStyle(hListView, LVS_EX_FULLROWSELECT);
+    lvc.mask = LVCF_TEXT | LVCF_SUBITEM | LVCF_WIDTH | LVCF_FMT;
+    lvc.fmt  = LVCFMT_CENTER;
+
+    EnableWindow(SendMessage(hListView,LVM_GETHEADER, 0, 0), FALSE);
+
+    lvc.iSubItem = 0;
+    lvc.cx       = 150;
+    lvc.pszText  = TEXT("Name");
+    ListView_InsertColumn(hListView, 0, &lvc);
+
+    lvc.iSubItem = 1;
+    lvc.cx       = 100;
+    lvc.pszText  = TEXT("Offset");
+    ListView_InsertColumn(hListView, 1, &lvc);
+
+    lvc.iSubItem = 2;
+    lvc.cx       = 100;
+    lvc.pszText  = TEXT("Size");
+    ListView_InsertColumn(hListView, 2, &lvc);
+}
+
+void ExtractAction(void)
+{
+    DWORD dwPos;
+
+    dwPos = ListView_GetNextItem(hListView, -1, LVNI_SELECTED);
+    if (dwPos == -1)
+        MessageBoxA(NULL, "Load a file first", "ERROR", 0);
+    else
+        ExtractFile(&sFile, dwPos);
+}
+
+void PopMenuList(HWND hWin, LPARAM lParam)
+{
+    HMENU hSubMenu;
+    LPNMHDR pnmhdr;
+    POINT lpPoint;
+
+    pnmhdr = (LPNMHDR)lParam;
+    if (pnmhdr->hwndFrom == hListView && pnmhdr->code == NM_RCLICK)
+    {
+        hSubMenu = CreatePopupMenu();
+        AppendMenu(hSubMenu, MF_STRING, ID_EXTRACT, "E&xtract");
+        GetCursorPos(&lpPoint);
+        TrackPopupMenu(hSubMenu, TPM_LEFTALIGN , lpPoint.x, lpPoint.y, NULL, hWin, NULL);
+    }
+}
+
+void OpenSndFile(HWND hWin)
 {
     OPENFILENAME ofn;
     char szFileName[MAX_PATH] = "";
 
     ZeroMemory(&ofn, sizeof(ofn));
-
     ofn.lStructSize = sizeof(ofn);
     ofn.hwndOwner = hWin;
     ofn.lpstrFilter = "Snd Files (*.snd)\0*.snd\0";
@@ -146,7 +154,6 @@ void open_snd(HWND hWin)
     ofn.nMaxFile = MAX_PATH;
     ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST;
     ofn.lpstrDefExt = "snd";
-
     ListView_DeleteAllItems(hListView);
     clean_file(&sFile);
     if (GetOpenFileName(&ofn))
@@ -154,9 +161,7 @@ void open_snd(HWND hWin)
         if (open_and_map(ofn.lpstrFile, &sFile) == FALSE)
             return;
         SetFileInformation(hWin, &ofn);
-        return;
     }
-    return;
 }
 
 void SetFileInformation(HWND hWin, OPENFILENAME *ofn)
@@ -177,19 +182,22 @@ void SetFileInformation(HWND hWin, OPENFILENAME *ofn)
     for (dwCount = 0; dwCount < GetNbEntry(&sFile); dwCount++)
     {
         sWav = (struct entry_wav *)((sFile.bMap + 4) + dwCount * sizeof (struct entry_wav));
-        add_item(dwCount, sWav);
+        AddItem(dwCount, sWav);
     }
 }
 
-void add_item(DWORD dwNum, struct entry_wav *sWav)
+void AddItem(DWORD dwNum, struct entry_wav *sWav)
 {
     LVITEM lv = {0};
     BYTE bSize[MAX_PATH];
     BYTE bOffset[MAX_PATH];
+    BYTE bName[40];
 
     lv.iItem = 0;
     ListView_InsertItem(hListView, &lv);
-    ListView_SetItemText(hListView, 0, 0, sWav->bName);
+    memcpy(bName, sWav->bName, 40);
+    FixWavNameFile(bName);
+    ListView_SetItemText(hListView, 0, 0, bName);
     sprintf(bOffset, "0x%X", sWav->dwOffset);
     ListView_SetItemText(hListView, 0, 1, bOffset);
     sprintf(bSize, "%d", sWav->dwSize);
@@ -204,31 +212,35 @@ DWORD GetNbEntry(struct file *sFile)
     return 0;
 }
 
-void extract_file(struct file *sFile, DWORD dwPos)
+void GetValue(DWORD dwItem, DWORD dwSubItem, PBYTE format, void *res)
 {
     BYTE buf[256];
     LV_ITEM itemInfo = {0};
+
+    itemInfo.iItem = dwItem;
+    itemInfo.iSubItem = dwSubItem;
+    itemInfo.mask = LVIF_TEXT;
+    itemInfo.cchTextMax = 256 - 1;
+    itemInfo.pszText = buf;
+    ListView_GetItem(hListView, &itemInfo);
+    sscanf(buf, format, res);
+}
+
+void ExtractFile(struct file *sFile, DWORD dwPos)
+{
     DWORD dwOffset;
     DWORD dwSize;
+    BYTE bName[40];
 
-    itemInfo.iItem = dwPos;
-    itemInfo.iSubItem = 1;
-    itemInfo.mask = LVIF_TEXT;
-    itemInfo.cchTextMax = 256;
-    itemInfo.pszText = buf;
-    ListView_GetItem(hListView, &itemInfo);
+    GetValue(dwPos, 1, "%X", &dwOffset);
+    GetValue(dwPos, 2, "%d", &dwSize);
+    GetValue(dwPos, 0, "%s", bName);
+    save_buf(bName, sFile->bMap + dwOffset, dwSize);
+}
 
-    sscanf(buf, "%X", &dwOffset);
-
-    itemInfo.iItem = dwPos;
-    itemInfo.iSubItem = 2;
-    itemInfo.mask = LVIF_TEXT;
-    itemInfo.cchTextMax = 256;
-    itemInfo.pszText = buf;
-    ListView_GetItem(hListView, &itemInfo);
-    sscanf(buf, "%d", &dwSize);
-
-    save_buf("wut.wav", sFile->bMap + dwOffset, dwSize);
-
-    //MessageBoxA(NULL, buf, buf, 0);
+void FixWavNameFile(PBYTE Buf)
+{
+    if (!Buf)
+        return;
+    Buf[strlen(Buf)] = '.';
 }
