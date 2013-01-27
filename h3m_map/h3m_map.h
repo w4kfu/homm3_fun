21f7886f23ed228968b3879ed5c95da2a67fc0ab
Samuel CHEVET <gw4kfu@gmail.com>
2013-01-27 17:44:43 +0100

    Adding zlib

---
+++ h3m_map/h3m_map.h
@@ -0,0 +1,17 @@
+#ifndef __H3M_MAP_H__
+#define __H3M_MAP_H__
+
+#pragma comment (lib, "comctl32.lib")
+#pragma comment (lib, "Comdlg32.lib")
+
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <commctrl.h>
+#include <Commdlg.h>
+#include <string.h>
+
+#include "file.h"
+
+#endif // __H3M_MAP_H__
--- h3m_map/main.c
+++ h3m_map/main.c
@@ -1,1 +1,12 @@
-#include "file.h"
+#include "h3m_map.h"
+
+HINSTANCE	hInst;
+
+int APIENTRY WinMainCRTStartup()
+{
+    hInst = (HINSTANCE)GetModuleHandleA(NULL);
+	InitCommonControls();
+	//DialogBoxParam(hInst, (LPCTSTR)IDD_DIALOG1, NULL, (DLGPROC)MainProc, 0);
+	ExitProcess (0);
+    return 0;
+}
