fe66355ae9eb849b98ca6bd0e8809fcfbd24890b
Samuel CHEVET <gw4kfu@gmail.com>
2013-01-28 16:37:17 +0100

    Adding palette support

--- pcx_viewer/main.c
+++ pcx_viewer/main.c
@@ -3,29 +3,13 @@
 HINSTANCE	hInst;
 LPTSTR		lpszDialogCaption = "PCX Viewer";
 struct      file sFile;
-HWND        hListView;
-
-LRESULT CALLBACK MainProc(HWND Dlg, UINT message, WPARAM wParam, LPARAM lParam);
-void OpenSndFile(HWND hWin);
-void SetFileInformation(HWND hWin, OPENFILENAME *ofn);
-void AddItem(DWORD dwNum, struct entry_wav *sWav);
-void ExtractFile(struct file *sFile, DWORD dwPos);
-DWORD GetNbEntry(struct file *sFile);
-void MakeMenu(HWND hWin);
-void ExtractAction(void);
-void PopMenuList(HWND hWin, LPARAM lParam);
-void FixWavNameFile(PBYTE Buf);
+HWND        picBoxDisp;
+struct file sFile;
 
 int APIENTRY WinMainCRTStartup()
 {
-    //INITCOMMONCONTROLSEX icex;
-
     hInst = (HINSTANCE)GetModuleHandleA(NULL);
 	InitCommonControls();
-
-    //icex.dwICC = ICC_LISTVIEW_CLASSES;
-    //InitCommonControlsEx(&icex);
-
 	DialogBoxParam(hInst, (LPCTSTR)IDD_DIALOG1, NULL, (DLGPROC)MainProc, 0);
 	ExitProcess (0);
     return 0;
@@ -53,7 +37,7 @@ LRESULT CALLBACK MainProc(HWND hWin, UINT message, WPARAM wParam, LPARAM lParam)
 			}
 			break;
         case WM_DROPFILES:
-            HandleFiles(wParam);
+            HandleFiles(hWin, wParam);
             break;
         case WM_CLOSE:
         case WM_DESTROY:
@@ -66,7 +50,12 @@ LRESULT CALLBACK MainProc(HWND hWin, UINT message, WPARAM wParam, LPARAM lParam)
    return TRUE;
 }
 
-void HandleFiles(WPARAM wParam)
+HBITMAP LoadBitmapFromBuffer(char *buffer, int width, int height)
+{
+    return CreateBitmap(width, height, 3, 12, buffer);
+}
+
+void HandleFiles(HWND hWin, WPARAM wParam)
 {
     BYTE bName[MAX_PATH];
     DWORD dwNbDrop;
@@ -75,8 +64,75 @@ void HandleFiles(WPARAM wParam)
     dwNbDrop = DragQueryFile(wParam, 0xFFFFFFFF, bName, MAX_PATH);
     for (dwCount = 0; dwCount < dwNbDrop; dwCount++)
     {
+        clean_file(&sFile);
         DragQueryFile(wParam, dwCount, bName, MAX_PATH);
-        //ExtractArchive(bName);
+        if (open_and_map(bName, &sFile) == FALSE)
+        {
+            clean_file(&sFile);
+            MessageBoxA(NULL, "[-] open_and_map failed", "error", 0);
+            return;
+        }
+        DrawPCX(hWin, bName);
     }
     DragFinish(wParam);
+}
+
+void GetRGB(struct h3pcx *homm3pcx, DWORD x, DWORD y, DWORD *r, DWORD *g, DWORD *b)
+{
+    int i;
+
+    if ((x < homm3pcx->dwWidth) && (y < homm3pcx->dwHeight))
+    {
+        i = (x + (y * homm3pcx->dwWidth)) * 3;
+        *b = homm3pcx->bBitmap[i + 0];
+        *g = homm3pcx->bBitmap[i + 1];
+        *r = homm3pcx->bBitmap[i + 2];
+    }
+    else
+    {
+        *r = 0;
+        *g = 0;
+        *r = 0;
+    }
+}
+
+void DrawPCX(HWND hWin, LPCTSTR FileName)
+{
+    struct h3pcx homm3pcx;
+    DWORD x, y;
+    HDC hdc = GetDC(hWin);
+    DWORD r, g, b;
+    DWORD dwIsPalette = 0;
+
+    homm3pcx.dwBitmapSize = *(DWORD*)sFile.bMap;
+    homm3pcx.dwWidth = *(DWORD*)(sFile.bMap + 4);
+    homm3pcx.dwHeight = *(DWORD*)(sFile.bMap + 8);
+    homm3pcx.bBitmap = (BYTE*)(sFile.bMap + 3 * sizeof(DWORD));
+
+    if (homm3pcx.dwBitmapSize != (homm3pcx.dwWidth * homm3pcx.dwHeight * 3))
+    {
+        dwIsPalette = 1;
+        homm3pcx.pal = (struct pcx_color*)(sFile.bMap + (3 * sizeof(DWORD)) + (homm3pcx.dwWidth * homm3pcx.dwHeight));
+    }
+
+    for (y = 0; y < homm3pcx.dwHeight; y++)
+    {
+        for (x = 0; x < homm3pcx.dwWidth; x++)
+        {
+            if (dwIsPalette)
+            {
+                struct pcx_color *pal;
+
+                pal = (struct pcx_color *)&homm3pcx.pal[homm3pcx.bBitmap[x + (y * homm3pcx.dwWidth)]];
+                SetPixel(hdc, x, y, RGB(pal->r, pal->g, pal->b));
+            }
+            else
+            {
+                /* BGR -> RGB */
+                GetRGB(&homm3pcx, x, y, &b, &g, &r);
+                SetPixel(hdc, x, y, RGB(r, g, b));
+            }
+        }
+    }
+    ReleaseDC(hWin, hdc);
 }
--- pcx_viewer/pcx_viewer.h
+++ pcx_viewer/pcx_viewer.h
@@ -16,7 +16,25 @@
 #include "file.h"
 #include "resource.h"
 
+struct pcx_color
+{
+	BYTE r;
+	BYTE g;
+	BYTE b;
+};
+
+struct h3pcx
+{
+  DWORD dwBitmapSize;
+  DWORD dwWidth;
+  DWORD dwHeight;
+  BYTE *bBitmap;
+  struct pcx_color *pal;
+};
+
 LRESULT CALLBACK MainProc(HWND hWin, UINT message, WPARAM wParam, LPARAM lParam);
-void HandleFiles(WPARAM wParam);
+void HandleFiles(HWND hWin, WPARAM wParam);
+void DrawPCX(HWND hWin, LPCTSTR FileName);
+void GetRGB(struct h3pcx *homm3pcx, DWORD x, DWORD y, DWORD *r, DWORD *g, DWORD *b);
 
 #endif // __PCX_VIEWER_H__
